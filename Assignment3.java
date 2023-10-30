3d8d88a958b4a38aaab847e35638c6b1f238e727
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-30 08:54:15 +0530

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -18,7 +18,6 @@ public class Assignment3 {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", ".\\chromedriver_win32\\chromedriver.exe");
-		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
